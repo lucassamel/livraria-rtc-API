12bb9b463387426670f2412ad69c07b9c8c90dfb
Lucas Samel <lucas.samel97@gmail.com>
2020-12-21 19:14:07 -0300

    Add project files.

---
+++ livraria-rtc/Model/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace livraria_rtc.Model
+{
+    public class Livro
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public int CRM { get; set; }
+
+    }
+}
